8b02dffe6a083615163c6e6419c1f4f656fc2f6e
Karynvo <Vkaryn@gmail.com>
2015-03-06 21:46:35 -0700

    created ShowMapActivity

---
+++ LetsDoIt/src/com/bearpluspie/letsdoit/ShowMapActivity.java
@@ -0,0 +1,46 @@
+package com.bearpluspie.letsdoit;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowMapActivity extends Activity{
+	private GoogleMap map;
+	static final LatLng TUCSON = new LatLng(32.221743, -110.926479);
+	static final LatLng PHOENIX = new LatLng(33.448377, -112.074037);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.show_map_layout);
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		
+		if(map != null){
+			Marker Tucson = map.addMarker(new MarkerOptions()
+			.position(TUCSON)
+			.title("Tucson")
+			.snippet("Tucson is lush"));
+			Marker Phoenix = map.addMarker(new MarkerOptions()
+			.position(PHOENIX)
+			.title("Phoenix title")
+			.snippet("Phoenix has lots of asphalt")
+			.icon(BitmapDescriptorFactory
+					.fromResource(R.drawable.ic_launcher)));
+			
+			//including some animation
+			//Move the camera instantly to Tucson with a zoom of 15.
+			map.moveCamera(CameraUpdateFactory.newLatLngZoom(TUCSON, 15));
+
+			// Zoom in, animating the camera.
+			map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		}
+	}
+}
